e20d93a8b1276312e794a775e91f54446d6ccf4e
user <user@EE09>
2020-09-29 09:40:46 +0800

    123

---
+++ src/lab/lab1222222.java
@@ -0,0 +1,13 @@
+package lab;
+
+public class lab1222222 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("I'm §d©Ó©þ");
+		System.out.println("My nid is D0843485");
+		System.out.println("THis is lab1 homework");
+		
+	}
+	
+}
